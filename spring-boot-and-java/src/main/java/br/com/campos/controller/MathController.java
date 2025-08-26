0c2dfaa9eb4c357e360ef61c3849d44c4a0262e2
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-26 18:29:49 -0300

    Chore: All calculator implemented

--- spring-boot-and-java/src/main/java/br/com/campos/controller/MathController.java
+++ spring-boot-and-java/src/main/java/br/com/campos/controller/MathController.java
@@ -20,6 +20,61 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping("/subtract/{numberOne}/{numberTwo}")
+    public Double subtract(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/mutiply/{numberOne}/{numberTwo}")
+    public Double multiply(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/divide/{numberOne}/{numberTwo}")
+    public Double divide(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        if (convertToDouble(numberOne) <= 0) {
+            throw new UnsupportedOperationException("Please, the number could not be zero or negative!");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping("/square/{numberOne}")
+    public Double square(
+            @PathVariable("numberOne") String numberOne
+    ) {
+        if (!isNumeric(numberOne))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        if (convertToDouble(numberOne) <= 0) {
+            throw new UnsupportedOperationException("Please, the number could not be zero or negative!");
+        }
+        return Math.sqrt(convertToDouble(numberOne));
+    }
+
     private Double convertToDouble(String strNumber) throws IllegalArgumentException {
         if (strNumber == null || strNumber.isEmpty())
             throw new UnsupportedOperationException("Please set a numeric value!");
@@ -33,6 +88,4 @@ public class MathController {
         String number = strNumber.replace(",", ".");
         return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
     }
-
-
 }
