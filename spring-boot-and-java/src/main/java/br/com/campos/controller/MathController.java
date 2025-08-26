80c9d7845070c6c407a8099d82acc851960e3c6f
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-26 16:43:45 -0300

    Chore: sum implemented in the mathController class

---
+++ spring-boot-and-java/src/main/java/br/com/campos/controller/MathController.java
@@ -0,0 +1,35 @@
+package br.com.campos.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// http://localhost:8080/math/sum/3/5
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty())  throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
+    }
+
+
+}
