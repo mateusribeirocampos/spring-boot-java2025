5eae52d9d5e144954a8bd52db78b0ab1686509bf
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-27 17:43:51 -0300

    Chore: refactoring to leave more clean

--- spring-boot-and-java/src/main/java/br/com/campos/math/SimpleMath.java
+++ spring-boot-and-java/src/main/java/br/com/campos/math/SimpleMath.java
@@ -4,22 +4,22 @@ import br.com.campos.converters.NumberConverter;
 
 public class SimpleMath {
 
-    public double sum(String numberOne, String numberTwo) {
-        return NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo);
+    public double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
     }
-    public double subtraction(String numberOne, String numberTwo) {
-        return NumberConverter.convertToDouble(numberOne) - NumberConverter.convertToDouble(numberTwo);
+    public double subtraction(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
     }
-    public double multiplication(String numberOne, String numberTwo) {
-        return NumberConverter.convertToDouble(numberOne) * NumberConverter.convertToDouble(numberTwo);
+    public double multiplication(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
     }
-    public double divide(String numberOne, String numberTwo) {
-        return NumberConverter.convertToDouble(numberOne) / NumberConverter.convertToDouble(numberTwo);
+    public double divide(Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
     }
-    public double mean(String numberOne, String numberTwo) {
-        return (NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo))/2;
+    public double mean(Double numberOne, Double numberTwo) {
+        return (numberOne + numberTwo)/2;
     }
-    public double square(String numberOne) {
-        return Math.sqrt(NumberConverter.convertToDouble(numberOne));
+    public double square(Double numberOne) {
+        return Math.sqrt(numberOne);
     }
 }
