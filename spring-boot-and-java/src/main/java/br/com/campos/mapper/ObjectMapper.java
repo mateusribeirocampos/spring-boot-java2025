d983379792c55bb878bab19ea7f64691d599eb99
mateusribeirocampos <mateusrcampos@gmail.com>
2025-09-02 17:05:28 -0300

    Test logs and DozerMapper - Implemented test log warn for application.yml - Implemented DozerMapper in the application

---
+++ spring-boot-and-java/src/main/java/br/com/campos/mapper/ObjectMapper.java
@@ -0,0 +1,24 @@
+package br.com.campos.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObject(List<O> origin, Class<D> destination) {
+        List<D> destinationObject = new ArrayList<>();
+        for (Object O : origin) {
+            destinationObject.add(mapper.map(origin, destination));
+        }
+        return destinationObject;
+    }
+}
