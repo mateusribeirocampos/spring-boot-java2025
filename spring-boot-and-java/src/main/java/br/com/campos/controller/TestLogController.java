5c1129ba6e45a2ab643a37b33698e309956b7819
mateusribeirocampos <mateusrcampos@gmail.com>
2025-09-04 17:21:08 -0300

    Chore: changing the @RequestMapping to new uri

--- spring-boot-and-java/src/main/java/br/com/campos/controller/TestLogController.java
+++ spring-boot-and-java/src/main/java/br/com/campos/controller/TestLogController.java
@@ -4,9 +4,11 @@ import br.com.campos.services.PersonServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(PersonServices .class.getName());
