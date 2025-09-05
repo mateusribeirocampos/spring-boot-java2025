88a63228c8a4d8b840c6364d87e1978178a5eec6
mateusribeirocampos <mateusrcampos@gmail.com>
2025-09-05 16:53:37 -0300

    chore: using annotation to format data and customized

--- spring-boot-and-java/src/main/java/br/com/campos/controller/PersonController.java
+++ spring-boot-and-java/src/main/java/br/com/campos/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,9 @@ public class PersonController {
     )
 
     public PersonDTO findById(@PathVariable("id") Long id) {
-        return service.findById(id);
+        var person = service.findById(id);
+        person.setBirthDay(new Date());
+        return person;
     }
 
     @GetMapping(
