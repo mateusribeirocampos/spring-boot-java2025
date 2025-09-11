f2247d99e62780cef831cc75909f6e000709253a
mateusribeirocampos <mateusrcampos@gmail.com>
2025-09-11 17:26:38 -0300

    Chore: content negotiation implemented

--- spring-boot-content-negotiation/src/main/java/br/com/campos/controller/PersonController.java
+++ spring-boot-content-negotiation/src/main/java/br/com/campos/controller/PersonController.java
@@ -3,6 +3,8 @@ package br.com.campos.controller;
 import br.com.campos.data.dto.v1.PersonDTO;
 import br.com.campos.data.dto.v2.PersonDTOV2;
 import br.com.campos.services.PersonServices;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import org.slf4j.Logger;
+
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -18,16 +22,20 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class);
+
     @GetMapping(value = "/{id}",
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE }
     )
 
-    public PersonDTO findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id, HttpServletRequest request) {
         var person = service.findById(id);
         person.setBirthDay(new Date());
         person.setPhoneNumber("+55 31 992359874");
         person.setSensitiveData("Foo bar");
-        return person;
+        logger.info("Accept Header: " + request.getHeader("Accept"));
+        //return person;
+        return service.findById(id);
     }
 
     @GetMapping(
