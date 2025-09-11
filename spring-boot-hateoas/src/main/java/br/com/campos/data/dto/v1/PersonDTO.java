02dbee6e5c5dacfa043f2d7d3854b8691fb7bc88
mateusribeirocampos <mateusrcampos@gmail.com>
2025-09-11 18:12:14 -0300

    Chore: HATEOAS implements with links to findById with all CRUD

---
+++ spring-boot-hateoas/src/main/java/br/com/campos/data/dto/v1/PersonDTO.java
@@ -0,0 +1,116 @@
+package br.com.campos.data.dto.v1;
+
+import com.fasterxml.jackson.annotation.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class PersonDTO extends RepresentationModel<PersonDTO> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date birthDay;
+
+    private String address;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
+
+    private String sensitiveData;
+
+    private String gender;
+
+    public PersonDTO() {
+    }
+    public PersonDTO(Long id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO personDTO)) return false;
+        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(birthDay, personDTO.birthDay) && Objects.equals(address, personDTO.address) && Objects.equals(phoneNumber, personDTO.phoneNumber) && Objects.equals(sensitiveData, personDTO.sensitiveData) && Objects.equals(gender, personDTO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, birthDay, address, phoneNumber, sensitiveData, gender);
+    }
+}
---
+++ spring-boot-hateoas/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Airton', 'Male', 'Senna'),
+	(3, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Mohamand', 'Male', 'Ali'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Manddela'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Manddela'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(12, 'Wurttemberg - Germany', 'Albert', 'Male', 'Einstein');
