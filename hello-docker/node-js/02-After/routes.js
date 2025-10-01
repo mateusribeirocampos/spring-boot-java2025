cc3dc7300229fac64be5a5fbd1e7118412b25eb9
mateusribeirocampos <mateusrcampos@gmail.com>
2025-10-01 17:45:25 -0300

    chore: directory deleted

--- hello-docker/node-js/02-After/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const DockerController = require('./controllers/DockerController')
-
-const routes = express.Router();
-
-module.exports = routes.get('/hello-docker', DockerController.greeting);
