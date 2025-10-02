6efe6ad8a9edd1ba545a8ab530260dd09cafa83d
mateusribeirocampos <mateusrcampos@gmail.com>
2025-10-02 14:46:20 -0300

    chore: Project to docker latest on the registry

---
+++ hello-docker/java-spring-boot/03-hello-docker-after/src/main/java/br/com/erudio/controller/DockerController.java
@@ -0,0 +1,38 @@
+package br.com.erudio.controller;
+
+import br.com.erudio.environment.InstanceInformationService;
+import br.com.erudio.model.HelloDocker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+	
+	Logger logger = LoggerFactory.getLogger(DockerController.class);
+
+	@Autowired
+	private InstanceInformationService service;
+
+	@GetMapping(path = "/")
+	public String imUpAndRunning() {
+		return "{healthy:true}";
+	}
+
+	@RequestMapping("/hello-docker")
+	public HelloDocker greeting() {
+		
+		logger.info("Endpoint /hello-docker is called!!!");
+
+        //var hostName= System.getenv("COMPUTER')
+        var hostName = System.getenv("HOSTNAME");
+
+		return new HelloDocker(
+	"Hello Docker - V2",
+			hostName
+		);
+	}
+}
