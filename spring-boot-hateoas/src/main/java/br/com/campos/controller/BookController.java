a65788672c6f22184ca6f5e4650b698a1a224251
mateusribeirocampos <mateusrcampos@gmail.com>
2025-10-02 17:32:43 -0300

    chore: Project spring-boot-hateoas with postgresql implemented

--- spring-boot-hateoas/src/main/java/br/com/campos/controller/BookController.java
+++ spring-boot-hateoas/src/main/java/br/com/campos/controller/BookController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/book/v1")
-@Tag(name = "Person", description = "Endpoints for managing person")
+@Tag(name = "Book", description = "Endpoints for managing book")
 public class BookController {
 
     @Autowired
---
+++ spring-boot-hateoas/src/main/resources/db/migration/postgresql/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO person (first_name, last_name, address, gender) VALUES
+	('Airton', 'Senna', 'São Paulo - Brasil', 'Male'),
+	('Leonardo', 'da Vinci', 'Anchiano - Italy', 'Male'),
+	('Indira', 'Gandhi', 'Porbandar - India', 'Female'),
+	('Mahatma', 'Gandhi', 'Porbandar - India', 'Male'),
+	('Mohamand', 'Ali', 'Kentucky - US', 'Male'),
+	('Nelson', 'Mandela', 'Mvezo - South Africa', 'Male'),
+	('Nikola', 'Tesla', 'Smiljan - Croatia', 'Male'),
+	('Albert', 'Einstein', 'Wurttemberg - Germany', 'Male');
