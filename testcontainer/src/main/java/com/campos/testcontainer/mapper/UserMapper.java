1129e15549547594144ae7cdc6f769c9e56ac856
mateusribeirocampos <mateusrcampos@gmail.com>
2025-11-04 16:57:01 -0300

    chore: UserMapper with toResponseListDto function and global exceptions implemented in the controllers

--- testcontainer/src/main/java/com/campos/testcontainer/mapper/UserMapper.java
+++ testcontainer/src/main/java/com/campos/testcontainer/mapper/UserMapper.java
@@ -4,6 +4,10 @@ import com.campos.testcontainer.data.dto.UserResponseDto;
 import com.campos.testcontainer.entities.User;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @Component
 public class UserMapper {
 
@@ -17,5 +21,13 @@ public class UserMapper {
                 entity.getGender(),
                 entity.getState()
         );
+    }
+
+    public List<UserResponseDto> toResponseListDto(List<User> entityList) {
+        if (entityList == null) return null;
+
+        return entityList.stream()
+                .map(this::toResponseDto)
+                .collect(Collectors.toList());
     }
 }
--- testcontainer/src/main/java/com/campos/testcontainer/services/UserService.java
+++ testcontainer/src/main/java/com/campos/testcontainer/services/UserService.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,5 +30,14 @@ public class UserService {
                 .orElseThrow(() -> new ResourceNotFoundException(id));
 
         return userMapper.toResponseDto(user);
+    }
+
+    public List<UserResponseDto> findAll() {
+        logger.info("Finding all users!");
+        List<User> user = userRepository.findAll();
+        for (User u : user) {
+            System.out.println(u.getPassword());
+        }
+        return userMapper.toResponseListDto(user);
     }
 }
